b5667e7f057c91da8dc5d06d5dbb450f63a68686
Vittoria <vittoriatosi2006@gmail.com>
2026-04-01 10:41:12 +0200

    aggiuna la struttura del componente

--- src/Recenti.tsx
+++ src/Recenti.tsx
@@ -1,5 +1,44 @@
 import "./Recenti.css";
 
 export default function Recenti() {
-  return <div> ciao</div>;
+  return (
+    <main className="recenti-container">
+      <h2 className="i-miei-pagamenti"> I MIEI PAGAMENTI </h2>
+      <div className="spesa-singola">
+        <img src="icona-acquisto.svg" className="icona-acquisto" />
+        <h3 className="descrizione-pagamento"> Caffè al bar </h3>
+        <h3 className="prezzo-pagamento"> € 1,50 </h3>
+      </div>
+      <div className="spesa-singola">
+        <img src="icona-acquisto.svg" className="icona-acquisto" />
+        <h3 className="descrizione-pagamento"> Caffè al bar </h3>
+        <h3 className="prezzo-pagamento"> € 1,50 </h3>
+      </div>
+      <div className="spesa-singola">
+        <img src="icona-acquisto.svg" className="icona-acquisto" />
+        <h3 className="descrizione-pagamento"> Caffè al bar </h3>
+        <h3 className="prezzo-pagamento"> € 1,50 </h3>
+      </div>
+      <div className="spesa-singola">
+        <img src="icona-acquisto.svg" className="icona-acquisto" />
+        <h3 className="descrizione-pagamento"> Caffè al bar </h3>
+        <h3 className="prezzo-pagamento"> € 1,50 </h3>
+      </div>
+      <div className="spesa-singola">
+        <img src="icona-acquisto.svg" className="icona-acquisto" />
+        <h3 className="descrizione-pagamento"> Caffè al bar </h3>
+        <h3 className="prezzo-pagamento"> € 1,50 </h3>
+      </div>
+      <div className="spesa-singola">
+        <img src="icona-acquisto.svg" className="icona-acquisto" />
+        <h3 className="descrizione-pagamento"> Caffè al bar </h3>
+        <h3 className="prezzo-pagamento"> € 1,50 </h3>
+      </div>
+      <div className="spesa-singola">
+        <img src="icona-acquisto.svg" className="icona-acquisto" />
+        <h3 className="descrizione-pagamento"> Caffè al bar </h3>
+        <h3 className="prezzo-pagamento"> € 1,50 </h3>
+      </div>
+    </main>
+  );
 }
